4b837f2f7ab425dae052f3471718140ae43c0d71
jake <jake.imanaka@gmail.com>
2021-10-26 01:13:19 -1000

    title and button spacing

--- sightsea/screens/Home/index.js
+++ sightsea/screens/Home/index.js
@@ -5,19 +5,46 @@ import { NavigationContainer } from "@react-navigation/native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    flexDirection: "column",
+    backgroundColor: "aliceblue",
     alignItems: "center",
-    justifyContent: "center",
+    //justifyContent: "center",
   },
+  titleText: {
+    fontSize: 50,
+    fontWeight: "bold",
+    textAlign: "center",
+    paddingTop: "10%"
+  },
+  buttonGroup: {
+    position: "relative",
+    marginTop: 10,
+    height: "20%",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  navButton : {
+    width: "100%",
+    marginTop: "5%",
+    height: "5%",
+  }
 });
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
+      <Text style={styles.titleText}>Welcome to SightSea!</Text>
+      <View style={styles.buttonGroup}>
       <Button
-        title="Report/Sight an animal"
+        style={styles.navButton}
+        title="Report an Animal Sighting"
         onPress={() => navigation.navigate("Form")}
       ></Button>
+      <Button
+        style={styles.navButton}
+        title="Report a Distressed Animal"
+      ></Button>
+      </View>
     </View>
   );
 };
