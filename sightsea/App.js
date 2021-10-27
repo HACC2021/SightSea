b428308286113d64f3dc6edecaabee8d493c350a
jake <jake.imanaka@gmail.com>
2021-10-27 01:37:07 -1000

    firebase finish initial setup

--- sightsea/App.js
+++ sightsea/App.js
@@ -6,6 +6,26 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SightForm from "./screens/Form";
 import HomeScreen from "./screens/Home";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBmMrLM-94w_FoUjeBJsNNzWDssfuGrfDQ",
+  authDomain: "sightsea-57e15.firebaseapp.com",
+  projectId: "sightsea-57e15",
+  storageBucket: "sightsea-57e15.appspot.com",
+  messagingSenderId: "540196098412",
+  appId: "1:540196098412:web:a53a48f2c8e7461ddf6349",
+  measurementId: "G-HPGWR7KS9T"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
