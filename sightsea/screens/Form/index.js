f5ed970aafea6cf38d2cdc19965707f2e01a61bb
jake <jake.imanaka@gmail.com>
2021-11-02 02:14:58 -1000

    remove datetime picker

--- sightsea/screens/Form/index.js
+++ sightsea/screens/Form/index.js
@@ -9,8 +9,8 @@ import {
   ScrollView,
 } from "react-native";
 //import DropDown from "react-native-paper-dropdown";
-import DateTimePicker from "@react-native-community/datetimepicker";
-//import MapView, { Marker } from "react-native-maps";
+//import DateTimePicker from "@react-native-community/datetimepicker";
+import MapView, { Marker } from "react-native-maps";
 //import Marker from "react-native-maps";
 
 import {
@@ -55,17 +55,13 @@ const styles = StyleSheet.create({
 const SightForm = () => {
   const [date, setDate] = React.useState(new Date());
   const [mode, setMode] = React.useState("date");
-  const [show, setShow] = React.useState(false);
+  const [showDate, setShowDate] = React.useState(false);
+  const [showTime, setShowTime] = React.useState(false);
   const [animalType, setAnimalType] = React.useState("Turtle");
   const [showAnimalDropDown, setShowAnimalDropDown] = React.useState(false);
   const [name, setName] = React.useState("");
   const [phoneNum, setPhoneNum] = React.useState("");
   const [validPhone, setValidPhone] = React.useState(false);
-  const animalTypes = [
-    { label: "Turtle", value: "turtle" },
-    { label: "Bird", value: "bird" },
-    { label: "Seal", value: "Seal" },
-  ];
   const [mapRegion, setmapRegion] = React.useState({
     latitude: 21.315603,
     longitude: -157.858093,
@@ -73,11 +69,7 @@ const SightForm = () => {
     longitudeDelta: 0.0421,
   });
 
-  const handleAnimalDropdown = () => {
-    setShowAnimalDropDown(!showAnimalDropDown);
-  };
-  const dropdownItemClick = (value) => {
-    setAnimalType(value);
+  const closeAnimalDropdown = () => {
     setShowAnimalDropDown(!showAnimalDropDown);
   };
 
@@ -108,140 +100,127 @@ const SightForm = () => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.form}>
-        <Headline>Report a Sighting</Headline>
-        <Subheading>
-          Fill out the form below to submit a sighting and our staffs will
-          review the submitted form shortly.
-        </Subheading>
-        {/*  datatimepicker must be wrapped in a view to work */}
-        <View>
-          <Title>Select Date and Time:</Title>
-          <View style={{ flexDirection: "row" }}>
-            <View style={{ flex: 0.5, flexDirection: "column" }}>
-              <DateTimePicker
-                testID="dateTimePicker"
-                value={date}
-                mode="date"
-                is24Hour={true}
-                display="default"
-                onChange={onChange}
-              />
-            </View>
-            <View style={{ flex: 0.5, flexDirection: "column" }}>
-              <DateTimePicker
-                testID="dateTimePicker"
-                value={date}
-                mode="time"
-                is24Hour={true}
-                display="default"
-                onChange={onChange}
-              />
-            </View>
+      <ScrollView>
+        <View style={styles.form}>
+          <Headline>Report a Sighting</Headline>
+          <Subheading>
+            Fill out the form below to submit a sighting and our staffs will
+            review the submitted form shortly.
+          </Subheading>
+          {/* datatimepicker must be wrapped in a view to work
+          <View>
+            <Title>Select Date and Time:</Title>
+            <View style={{ flexDirection: "row" }}>
+              <View style={{ flex: 0.5, flexDirection: "column" }}>
+                <DateTimePicker
+                  testID="dateTimePicker"
+                  value={date}
+                  mode="date"
+                  is24Hour={true}
+                  display={showDate}
+                  onChange={onChange}
+                />
+              </View>
+              <View style={{ flex: 0.5, flexDirection: "column" }}>
+                <DateTimePicker
+                  testID="dateTimePicker"
+                  value={date}
+                  mode="time"
+                  is24Hour={true}
+                  display={showTime}
+                  onChange={onChange}
+                />
+              </View>
+           </View>
+           </View> */}
+          {/* <Marker coordinate={(37, -122)} /> */}
+
+          <View>
+            <MapView style={styles.map} region={mapRegion}>
+              <Marker key={0} coordinate={mapRegion} title={"Marker"} />
+            </MapView>
           </View>
-        </View>
-        {/* <Marker coordinate={(37, -122)} /> */}
-        {/*
-        <View>
-          <MapView style={styles.map} region={mapRegion}>
-            <Marker key={0} coordinate={mapRegion} title={"Marker"} />
-          </MapView>
-        </View>
-        */}
 
-        {/* <View>
-          <DropDown
-            style={styles.input}
-            label={"Animal Type"}
-            mode={"outlined"}
-            visible={showAnimalDropDown}
-            showDropDown={() => setShowAnimalDropDown(true)}
-            onDismiss={() => setShowAnimalDropDown(false)}
-            value={animalType}
-            setValue={setAnimalType}
-            list={animalTypes}
-          />
-        </View> */}
-
-        <View>
-          <List.Section title="Animal Accordian">
-            <List.Accordion
-              title={animalType}
-              expanded={showAnimalDropDown}
-              onPress={handleAnimalDropdown}
-            >
-              <List.Item
-                title="Turtle"
-                onPress={function () {
-                  setAnimalType("Turtle");
-                  handleAnimalDropdown();
-                }}
-              />
-              <List.Item
-                title="Seal"
-                onPress={function () {
-                  setAnimalType("Seal");
-                  handleAnimalDropdown();
-                }}
-              />
-              <List.Item
-                title="Bird"
-                onPress={function () {
-                  setAnimalType("Bird");
-                  handleAnimalDropdown();
-                }}
-              />
-            </List.Accordion>
-          </List.Section>
-          <TextInput
-            style={styles.input}
-            onChangeText={setName}
-            value={name}
-            mode="outlined"
-            textContentType="name"
-            label="Enter First Name"
-          />
-          <TextInput
-            style={styles.input}
-            onChangeText={setPhoneNum}
-            mode="outlined"
-            keyboardType="decimal-pad"
-            label="Phone number"
-          />
-          <TextInput
-            style={styles.input}
-            mode="outlined"
-            label="Is the animal on the beach or in the water?"
-          />
-          {animalType === "Seal" ? null : (
-            <View>
-              <TextInput
-                style={styles.input}
-                mode="outlined"
-                label="Describe any visible wounds (size/color)"
-              />
-              <TextInput
-                style={styles.input}
-                mode="outlined"
-                label="Describe any previous wounds (ex. amputated flipper)"
-              />
-              <TextInput
-                style={styles.input}
-                mode="outlined"
-                label="Describe the animal's behavior (ex. is it lethargic?)"
-              />
-              <TextInput
-                style={styles.input}
-                mode="outlined"
-                label="About what size is the animal?"
-              />
-            </View>
-          )}
+          <View>
+            <List.Section title="Animal Type">
+              <List.Accordion
+                title={animalType}
+                expanded={showAnimalDropDown}
+                onPress={closeAnimalDropdown}
+              >
+                <List.Item
+                  title="Turtle"
+                  onPress={function () {
+                    setAnimalType("Turtle");
+                    closeAnimalDropdown();
+                  }}
+                />
+                <List.Item
+                  title="Seal"
+                  onPress={function () {
+                    setAnimalType("Seal");
+                    closeAnimalDropdown();
+                  }}
+                />
+                <List.Item
+                  title="Bird"
+                  onPress={function () {
+                    setAnimalType("Bird");
+                    closeAnimalDropdown();
+                  }}
+                />
+              </List.Accordion>
+            </List.Section>
+            <TextInput
+              style={styles.input}
+              onChangeText={setName}
+              value={name}
+              mode="outlined"
+              textContentType="name"
+              label="Enter First Name"
+            />
+            <TextInput
+              style={styles.input}
+              onChangeText={setPhoneNum}
+              mode="outlined"
+              keyboardType="decimal-pad"
+              label="Phone number"
+            />
+            <TextInput
+              style={styles.input}
+              mode="outlined"
+              label="Is the animal on the beach or in the water?"
+            />
+            {animalType === "Seal" ? null : (
+              <View>
+                <TextInput
+                  style={styles.input}
+                  mode="outlined"
+                  label="Describe any visible wounds (size/color)"
+                />
+                <TextInput
+                  style={styles.input}
+                  mode="outlined"
+                  label="Describe any previous wounds (ex. amputated flipper)"
+                />
+                <TextInput
+                  style={styles.input}
+                  mode="outlined"
+                  label="Describe the animal's behavior (ex. is it lethargic?)"
+                />
+                <TextInput
+                  style={styles.input}
+                  mode="outlined"
+                  label="About what size is the animal?"
+                />
+              </View>
+            )}
+          </View>
+          <Button style={styles.btn} mode="contained" onPress={handleSubmit}>
+            Submit
+          </Button>
         </View>
-        <Button style={styles.btn} mode="contained" onPress={handleSubmit}>
-          Submit
-        </Button>
-      </View>
+      </ScrollView>
     </View>
   );
 };
