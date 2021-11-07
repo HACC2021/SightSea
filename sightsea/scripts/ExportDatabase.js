9eff7dd1692a3c19df4cde58ef721b259f59a7cc
jake <jake.imanaka@gmail.com>
2021-11-06 20:02:10 -1000

    get document from database

--- sightsea/scripts/ExportDatabase.js
+++ sightsea/scripts/ExportDatabase.js
@@ -2,13 +2,12 @@ import React from "react";
 import { getDatabase, ref, onValue, once } from "firebase/database";
 
 const ExportDatabase = () => {
-  //   var dataJson = {};
-  //   const db = getDatabase();
-  //   const reference = ref(db, "/Bird");
-  //   onValue(reference, (snapshot) => {
-  //     console.log(snapshot.val());
-  //   });
-  window.alert("hi");
+  var dataJson = {};
+  const db = getDatabase();
+  const reference = ref(db, "/Bird");
+  onValue(reference, (snapshot) => {
+    console.log(snapshot.val());
+  });
 };
 
 export default ExportDatabase;
